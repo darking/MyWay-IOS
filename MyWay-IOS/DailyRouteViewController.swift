a9a3cd4e73f4c0ebf3dc461c368ec9545cbe12ee
BaderABehbehani <trn14@trn14s-mbp.trn.pifss.gov.kw>
2015-08-13 09:47:15 +0300

    Group 5

---
+++ MyWay-IOS/DailyRouteViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DailyRouteViewController.swift
+//  MyWay-IOS
+//
+//  Created by trn22 on 8/13/15.
+//  Copyright (c) 2015 PIFSS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DailyRouteViewController: UIViewController {
+    @IBOutlet weak var FromDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var ToDatePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
