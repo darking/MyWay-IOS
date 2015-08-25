98c2f3b60e764d29a6c3d54f14ce67a634ea5a1f
Abdullah Al Mashmoum <darkling_q8@hotmail.com>
2015-08-25 17:37:14 +0300

    mashmoum

--- SwiftSideMenu/mainViewController.swift
+++ SwiftSideMenu/mainViewController.swift
@@ -13,6 +13,8 @@ import GoogleMaps
 
 class mainViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate, ENSideMenuDelegate {
 
+    //
+    
     var actionSheet = AHKActionSheet()
     
     var favBool:Bool = false
