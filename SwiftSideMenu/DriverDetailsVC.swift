5f9ad1876bf633a8520d6b25936e29682c8756bf
AdnanQaz <adnanqaz@live.com>
2015-10-06 00:04:45 +0300

    Change Log: 5. Fix view driver details: add driver image, add driver name, add driver username, current location, battery status, btnSetDestination, and add driver report table to second half of screen 6. Remove View Reports button 6.1. Removed DaysListTVC screen 6.2. Removed corresponding IBAction 6.3. Removed View Report screen 6.4. Removed unneeded custom tableview, custom tableviewcontroller 6.5. Remove Get current location button 6.6. Added if statement to ShowOnMapVC to determine if the command came from the parent or not in order to set the coordinates for the driver 6.7. Added UIAlertController to notify the parent that the new destination was sent

--- SwiftSideMenu/DriverDetailsVC.swift
+++ SwiftSideMenu/DriverDetailsVC.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
-
+import GoogleMaps
 class DriverDetailsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var lblDriverName: UILabel!
     var driverName = "";
-    
+    //let settingDriverDestination = true;
     @IBOutlet weak var driverImage: UIImageView!
     @IBOutlet weak var reportsTable: UITableView!
     @IBOutlet weak var lblDriverLocation: UILabel!
     @IBOutlet weak var lblBatteryStatus: UILabel!
+    @IBAction func btnSetDestination(sender: AnyObject) {
+        
+        DriverDetailsVC.SettingDriverDestination.settingDriverDestination = true;
+        var showMap:ShowOnMapVC = UIStoryboard(name: "reqLocaiton", bundle: nil).instantiateViewControllerWithIdentifier("ShowOnMapVC") as! ShowOnMapVC;
+        showMap.title = "Set Driver Destination";
+        showMap.message = "Please select a location to send to the driver";
+        var driverDestination:CLLocation = CLLocation(latitude: 29.282064, longitude: 47.994);
+      //Zahraa st 305 (29.282064 , 47.994)
+        
+        showMap.location = driverDestination;
+        let (latitud, longitud) =  GetLocationVC().getCurrentLocation()
+  
+        self.navigationController?.pushViewController(showMap, animated: true);
+    }
     
   
     override func viewDidLoad() {
@@ -34,7 +48,22 @@ class DriverDetailsVC: UIViewController, UITableViewDelegate, UITableViewDataSou
         //Read battery percntage from driver's phone
         lblBatteryStatus.text = "50%";
         
+        
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        if (DriverDetailsVC.SettingDriverDestination.settingDriverDestination == true) {
+        var notifyNewDestinationSet:UIAlertController = UIAlertController(title: "Destination Set", message: "Your desired destination has been set to the driver!", preferredStyle: .Alert);
+            let okay:UIAlertAction = UIAlertAction(title: "Okay", style: .Default){ action -> Void in
+                //Do some other stuff
+            }
+;            notifyNewDestinationSet.addAction(okay);
+        self.presentViewController(notifyNewDestinationSet, animated: true, completion: nil);
+        DriverDetailsVC.SettingDriverDestination.settingDriverDestination = false;
+        }
+
+    }
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1;
     }
@@ -48,6 +77,18 @@ class DriverDetailsVC: UIViewController, UITableViewDelegate, UITableViewDataSou
         
         return cell;
     }
-    
+    struct SettingDriverDestination {
+        static var settingDriverDestination = false;
+        
+        static func setDriverLatLong(driverLatLong:CLLocationCoordinate2D) -> CLLocation {
+            let lat = driverLatLong.latitude;
+            let long = driverLatLong.longitude;
+            
+            let driverLatLong:CLLocation = CLLocation(latitude: lat, longitude: long);
+            
+            return driverLatLong;
+        }
+        
+    }
    
 }
