b969689f5a5c788fe8eaea29ae1843bd3c48a576
hebah303 <Scientist@hebahs-MacBook-Pro.local>
2015-10-07 14:31:14 +0300

    change get to post in team 4

--- SwiftSideMenu/URLCommentConnection.swift
+++ SwiftSideMenu/URLCommentConnection.swift
@@ -18,10 +18,17 @@ class URLCommentConnection: UIViewController, NSURLConnectionDelegate, NSURLConn
         
         var URLStatement:NSString = cl.urlParams(CommentLocation().valuesDicForComment(comment,id_report: id_report));
         
-        URLStatement.dataUsingEncoding(NSUTF8StringEncoding);
-         var urlStr : NSString = URLStatement.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        //URLStatement.dataUsingEncoding(NSUTF8StringEncoding);
         
-        let Group4Url:NSURL?=NSURL(string:"http://mobile.comxa.com/reports/all_reports.json?\(urlStr)");
+        // var urlStr : NSString = URLStatement.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        
+       // let Group4Url:NSURL?=NSURL(string:"http://mobile.comxa.com/reports/all_reports.json?\(urlStr)");
+        
+        
+        let Group4Url:NSURL?=NSURL(string:"http://mobile.comxa.com/reports/all_reports.json?");
+        let request = NSMutableURLRequest(URL:Group4Url!);
+        request.HTTPMethod="POST";
+        request.HTTPBody=URLStatement.dataUsingEncoding(NSUTF8StringEncoding);
         
         let urlReq:NSURLRequest=NSURLRequest(URL:Group4Url!);
         let connection:NSURLConnection?=NSURLConnection(request: urlReq, delegate: self, startImmediately: true);
